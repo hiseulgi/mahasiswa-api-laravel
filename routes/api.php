5f69816d70747a930fff5d38e84fcd3f19f00985
HiSeulgi <sugabida@gmail.com>
2022-06-21 17:32:26 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/mahasiswa', MahasiswaController::class);
+Route::resource('/', MahasiswaController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
